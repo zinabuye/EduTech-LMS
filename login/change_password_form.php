2ee68cf0a159da0f84a1eb4c69b12309e51f32f6
Zinabu Teka Tseghay <45485101+zinabuye@users.noreply.github.com>
2019-06-10 11:56:05 +0300

    ብትግርኛ
    ካብ እንግሊዘኛ ናብ ትግርኛ ምቕያር

--- login/change_password_form.php
+++ login/change_password_form.php
@@ -1,28 +1,5 @@
 <?php
-
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Change password form definition.
- *
- * @package    core
- * @subpackage auth
- * @copyright  2006 Petr Skoda {@link http://skodak.org}
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+//እዙይ ናይ መምሀሪ ፕላትፎርም እዙይ ብ ተምሃሮ MIT ብዝተወሰነ መልክዑ ተዳልዩ ዝቐረበ እዩ።
 
 defined('MOODLE_INTERNAL') || die();
 
