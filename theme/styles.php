a42c4cb74f5986185d65eb3ed409ffc948d954f7
Zinabu Teka Tseghay <45485101+zinabuye@users.noreply.github.com>
2019-06-10 12:07:24 +0300

    The style should be greenish
    The greenish theme is always ideal for schooling to attract students

--- theme/styles.php
+++ theme/styles.php
@@ -1,29 +1,4 @@
-<?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * This file is responsible for serving the one huge CSS of each theme.
- *
- * @package   core
- * @copyright 2009 Petr Skoda (skodak)  {@link http://skodak.org}
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
-// Disable moodle specific debug messages and any errors in output,
-// comment out when debugging or better look into error log!
 define('NO_DEBUG_DISPLAY', true);
 
 define('ABORT_AFTER_CONFIG', true);
@@ -51,7 +26,7 @@ if ($slashargument = min_get_slash_argument()) {
     }
 
     list($themename, $rev, $type) = explode('/', $slashargument, 3);
-    $themename = min_clean_param($themename, 'SAFEDIR');
+    $themename = min_clean_param($themename, 'SAFEDIRE');
     $rev       = min_clean_param($rev, 'RAW');
     $type      = min_clean_param($type, 'SAFEDIR');
 
